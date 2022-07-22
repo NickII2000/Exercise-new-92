6f75f4cba32674a6086c771fdb3d06385f4e5bfc
Nick <Nick_II2000@mail.ru>
2022-07-22 07:43:55 +0600

    6-th commit

--- js/script.js
+++ js/script.js
@@ -361,8 +361,38 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Slider (Exercise 92)
 
-    const slider = document.querySelectorAll('.offer__slide'),
+    const slides = document.querySelectorAll('.offer__slide'),
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('offer__slider-next');
+    let slideIndex = 1;
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach(item => item.style.display = 'none');
+
+        slides[slideIndex - 1].style.display = 'block';
+
+    }
+
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', () => {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', () => {
+        plusSlides(1);
+    });
 
 });
